0d88333c21b0dc1c5d3bf7fa0d8812c73ffcdfa6
Alexandre Nicolaie <xunleii@users.noreply.github.com>
2024-03-22 18:25:42 +0100

    :bricks: (live): Configure AWS SES
    AWS SES will be used as SMTP provider for this project.
    
    NOTE: all recipients must be pre-allowed by adding the mail into
    `allowed_identities` then follwoing the received email to allow SES to
    send email to these recipient
    
    Signed-off-by: Alexandre Nicolaie <xunleii@users.noreply.github.com>

---
+++ infrastructure/live/versions.tf
@@ -0,0 +1,28 @@
+# Copyright 2024
+#
+# Everyone is permitted to copy, distribute, modify, merge, sell, publish,
+# sublicense or whatever the fuck they want with this software but at their
+# OWN RISK.
+# The author has absolutely no fucking clue what the code in this project
+# does. It might just fucking work or not, there is no third option.
+#
+# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+# ---
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.37.0"
+    }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "4.23.0"
+    }
+  }
+
+  required_version = ">= 1.7.0"
+}
