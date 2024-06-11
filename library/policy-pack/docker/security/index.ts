46cd6d29940b2d40eb23a4c7a611310314bda6b8
Alexandre Nicolaie <xunleii@users.noreply.github.com>
2024-06-11 22:24:18 +0200

    :sparkles:(policy-pack): Add policies to enforce Docker best-practices
    Signed-off-by: Alexandre Nicolaie <xunleii@users.noreply.github.com>

---
+++ library/policy-pack/docker/security/index.ts
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2024 Alexandre Nicolaie (xunleii@users.noreply.github.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ----------------------------------------------------------------------------
+ */
+
+import { PolicyPack, ReportViolation, StackValidationArgs, validateResourceOfType } from "@pulumi/policy";
+
+import * as snyk from "./snyk";
+
+const policies = new PolicyPack("docker-security", {
+    enforcementLevel: "mandatory",
+    policies: [
+        // -- Policy to scan Docker images for vulnerabilities using Snyk
+        {
+            name: "snyk-container-scan",
+            description: "Scans Docker Images with Snyk",
+            enforcementLevel: "mandatory",
+            configSchema: {
+                properties: {
+                    excludeBaseImageVulns: {
+                        default: false,
+                        type: "boolean",
+                    },
+                    severityThreshold: {
+                        default: "critical",
+                        enum: ["low", "medium", "high", "critical"],
+                    },
+                },
+            },
+            validateStack: async (args: StackValidationArgs, reportViolation: ReportViolation) => {
+                const opts = args.getConfig<snyk.ScanOpts>();
+
+                const legacyImages = args.resources.filter(r => r.type === "docker:index/image:Image").map(r => r.props.imageName as string);
+                const buildxImages = args.resources.filter(r => r.type === "docker-build:index:Image").map(r => r.props.ref as string);
+                const images = [...legacyImages, ...buildxImages];
+
+                let scan: Array<Promise<void>> = [];
+                for (const image of images) {
+                    scan.push(
+                        snyk.scanImage(image, opts)
+                            .catch((e: any) => reportViolation(e))
+                    );
+                }
+
+                await Promise.all(scan);
+            },
+        }
+    ]
+});
---
+++ library/policy-pack/docker/security/snyk.ts
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2024 Alexandre Nicolaie (xunleii@users.noreply.github.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ----------------------------------------------------------------------------
+ */
+
+import { spawn } from "child_process";
+
+/**
+ * Enum of possible Snyk severity thresholds.
+ */
+export declare enum SeverityThreshold {
+    Low = "low",
+    Medium = "medium",
+    High = "high",
+    Critical = "critical",
+}
+
+/**
+ * Options for the Snyk scan.
+ */
+export interface ScanOpts {
+    /**
+     * Whether to exclude base image vulnerabilities.
+     */
+    excludeBaseImageVulns: boolean;
+
+    /**
+     * The severity threshold to fail on.
+     */
+    severityThreshold: SeverityThreshold;
+}
+
+/**
+ * Scans Docker images for vulnerabilities using Snyk.
+ */
+export async function scanImage(image: string, opts?: ScanOpts): Promise<void> {
+    const commandArgs = ["container", "test", image];
+    if (opts?.excludeBaseImageVulns) {
+        commandArgs.push("--exclude-base-image-vulns");
+    }
+    commandArgs.push(`--severity-threshold=${opts?.severityThreshold ?? SeverityThreshold.Critical}`);
+
+    await new Promise<void>((resolve, reject) => {
+        const child = spawn("snyk", commandArgs)
+        const stdout: string[] = [];
+        const stderr: string[] = [];
+
+        child.stdout.on("data", (data) => { stdout.push(data.toString()); });
+        child.stderr.on("data", (data) => { stderr.push(data.toString()); });
+
+        child.on("error", (err) => {
+            reject(`Snyk validation failed: ${err}`);
+        });
+        child.on("exit", (code) => {
+            if (code !== 0) {
+                let err = `Snyk validation failed with code ${code}`;
+                if (stdout) { err += `\n${stdout.join("\n")}`; }
+                if (stderr) { err += `\n${stderr.join("\n")}`; }
+
+                reject(err);
+            } else {
+                resolve();
+            }
+        });
+    });
+}
