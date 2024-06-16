e61e0c5e8b09cc9ee9fa0d280cea04358166df5e
Alexandre Nicolaie <xunleii@users.noreply.github.com>
2024-06-16 19:05:09 +0200

    :art:(gh): Configure .trunk and format all files
    Signed-off-by: Alexandre Nicolaie <xunleii@users.noreply.github.com>

--- catalog/os/alpine/3.19/docker.ts
+++ catalog/os/alpine/3.19/docker.ts
@@ -22,24 +22,33 @@ import { types, LocalImage } from "@chezmoi.sh/core/docker";
 
 // renovate: datasource=docker depName=alpine versioning=semver
 export const Version = "3.19.1";
-const shasum = "sha256:c5b1261d6d3e43071626931fc004f70149baeba2c8ec672bd4f27761f8e1ad6b";
+const shasum =
+  "sha256:c5b1261d6d3e43071626931fc004f70149baeba2c8ec672bd4f27761f8e1ad6b";
 
 /**
  * Alpine is a lightweight Linux distribution based on musl libc and BusyBox.
  */
 export class Image extends LocalImage {
-    constructor(name: string, args: types.ImageArgs, opts?: pulumi.ComponentResourceOptions) {
-        super(name, {
-            ...{ tags: [`oci.chezmoi.sh/os/alpine:${Version}`] },
-            ...args,
+  constructor(
+    name: string,
+    args: types.ImageArgs,
+    opts?: pulumi.ComponentResourceOptions,
+  ) {
+    super(
+      name,
+      {
+        ...{ tags: [`oci.chezmoi.sh/os/alpine:${Version}`] },
+        ...args,
 
-            // Build the image
-            context: { location: __dirname },
-            dockerfile: { location: path.join(__dirname, "Dockerfile") },
-            buildArgs: {
-                ALPN_VERSION: Version,
-                ALPN_VERSION_SHASUM: shasum,
-            },
-        }, opts);
-    }
+        // Build the image
+        context: { location: __dirname },
+        dockerfile: { location: path.join(__dirname, "Dockerfile") },
+        buildArgs: {
+          ALPN_VERSION: Version,
+          ALPN_VERSION_SHASUM: shasum,
+        },
+      },
+      opts,
+    );
+  }
 }
