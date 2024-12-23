548af45c88fcaf4b320894f2f91406bbbe620f26
Alexandre Nicolaie <xunleii@users.noreply.github.com>
2024-12-23 15:54:08 +0100

    :rocket:(project:maison): Install Tailscale operator
    Signed-off-by: Alexandre Nicolaie <xunleii@users.noreply.github.com>

---
+++ projects/chezmoi.sh/src/kubevault/kvstore.enc/cloud/tailscale/kubernetes.nx.chezmoi.sh
@@ -0,0 +1,23 @@
+#ENC[AES256_GCM,data:rG7sOwC94HutBEmui9THfLpeiaNpbHL53TYC7ifOgfYrdplvmy64EgNBDF2K5FoF26+H4MMH9lNzoBblVqHy1D0TNkYDdETdDM1pLXyXx84FNHb32Q==,iv:qPoc8BLOqKIaWU45v5iNO3fF4Xv29WqtVCrtO0Sa9ww=,tag:FKgn2sojv7DRjdjWTnbjwQ==,type:comment]
+operator-oauth-client-id: ENC[AES256_GCM,data:EleGa/qMt+ykza99GB1Npa4=,iv:cOFQ2z3qaNhpdlG/y6UbZDL6CVNahdu+r4QdGyZmZPg=,tag:HYuTUfTeE5yqeQWyHVGsOg==,type:str]
+operator-oauth-client-secret: ENC[AES256_GCM,data:Y/txwtkjH716YX9iloiAIe9jHVnhZOf/0hUtzXc47F7H9geZ4C6j9TNOXmuc8eGdFIAbIJ28UYaCqMWffygZFA==,iv:jE2Jn03mCoptT5WBwO4LUMCQ/nP22eo0NspyCu2zIII=,tag:wPegbuGY/ZCVrDucMnPSjg==,type:str]
+sops:
+    kms: []
+    gcp_kms: []
+    azure_kv: []
+    hc_vault: []
+    age:
+        - recipient: age1fj0yj3na3n5udfjmnxfwrlkp80tvj49w80wh699x33dh48clnvnshtjxe9
+          enc: |
+            -----BEGIN AGE ENCRYPTED FILE-----
+            YWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBUeGdSaHk4Tks0a3BZZVc5
+            YWxueHIxalR1NXpkQ1BJNUdYT29ya2dneFQ0CktWRTRTaU9TdFhPTTl1Uk5EdGxL
+            UFVneVRrOUNyWDNUU1cyWHY3UGNFZ0kKLS0tIEF2VzFyVERLRUt6bFpJZWNwNVJH
+            K2RLbGRCUGw1blhRZWpYbGpxdmdncXcKEEYlVS2i1lxYN4kzS554u4r33djDKr3v
+            Id/yfM2enaKZ7z0vGKTLnDJ7ik/ND5GmPblQNpf3bxuA3w2cCOv4KA==
+            -----END AGE ENCRYPTED FILE-----
+    lastmodified: "2024-12-23T15:35:51Z"
+    mac: ENC[AES256_GCM,data:nzYnO8b+IVsGaU1S3cXqJUI4oF7x84ZAKNsh1C1FSWX9mDemM1u1Tjbt6w53Q4rLUoA/egSjTqYfZvI8GBev5cy60RYIqcx40R3spbuYpRagbJFXRvZ9DSuLwMTLoKHs7OEgYq62nZJ3ZpzKYzJgjXjMZIcm23PTmcdSubf/oUE=,iv:ILGkaBr1xSEpMrhm2zC8BPA8hxzJ/3Ld6xjtWXAd2vs=,tag:H1AJAC9+MT6AJtMBOLwx6Q==,type:str]
+    pgp: []
+    unencrypted_suffix: _unencrypted
+    version: 3.9.0
