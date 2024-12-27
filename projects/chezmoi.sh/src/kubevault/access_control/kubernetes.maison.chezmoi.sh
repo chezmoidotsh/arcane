cd91382c587216a1621738a70dddb80a16464e7b
Alexandre Nicolaie <xunleii@users.noreply.github.com>
2024-12-27 19:33:43 +0100

    :rocket:(project:maison): Install Linkding application
    Add the raw client secret inside the OIDC configuration
    
    Signed-off-by: Alexandre Nicolaie <xunleii@users.noreply.github.com>

--- projects/chezmoi.sh/src/kubevault/access_control/kubernetes.maison.chezmoi.sh
+++ projects/chezmoi.sh/src/kubevault/access_control/kubernetes.maison.chezmoi.sh
@@ -2,4 +2,5 @@
 # credentials required by system services (ExternalDNS, cert-manager, etc.)
 cloud/letsencrypt
 cloud/tailscale/kubernetes.maison.chezmoi.sh
+security/sso/oidc/clients/linkding
 storage/minio/cnpg.maison.chezmoi.sh
