c0f51ed0667d06a15c2e88b37929429d58136490
Alexandre Nicolaie <xunleii@users.noreply.github.com>
2024-12-30 11:26:43 +0100

    :rocket:(project:maison): Install Mealie application
    Signed-off-by: Alexandre Nicolaie <xunleii@users.noreply.github.com>

--- projects/chezmoi.sh/src/kubevault/access_control/kubernetes.maison.chezmoi.sh
+++ projects/chezmoi.sh/src/kubevault/access_control/kubernetes.maison.chezmoi.sh
@@ -2,5 +2,7 @@
 # credentials required by system services (ExternalDNS, cert-manager, etc.)
 cloud/letsencrypt
 cloud/tailscale/kubernetes.maison.chezmoi.sh
+cloud/openai/mealie
 security/sso/oidc/clients/linkding
+security/sso/oidc/clients/mealie
 storage/minio/cnpg.maison.chezmoi.sh
