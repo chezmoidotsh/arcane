8ba1c4381e49709420f12c7add38298e3e67a3b1
Alexandre Nicolaie <xunleii@users.noreply.github.com>
2025-01-01 21:00:20 +0100

    :rocket:(project:maison): Install Paperless NGX application
    Signed-off-by: Alexandre Nicolaie <xunleii@users.noreply.github.com>

--- projects/chezmoi.sh/src/kubevault/access_control/kubernetes.maison.chezmoi.sh
+++ projects/chezmoi.sh/src/kubevault/access_control/kubernetes.maison.chezmoi.sh
@@ -5,4 +5,6 @@ cloud/tailscale/kubernetes.maison.chezmoi.sh
 cloud/openai/mealie
 security/sso/oidc/clients/linkding
 security/sso/oidc/clients/mealie
+security/sso/oidc/clients/paperless-ngx
 storage/minio/cnpg.maison.chezmoi.sh
+storage/smb/paperless-ngx
