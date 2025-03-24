01f587d3512be1d532ffef76bdaf388e6b088aff
Alexandre Nicolaie <xunleii@users.noreply.github.com>
2025-03-24 22:05:06 +0100

    :truck:(project:nex·rpi): Move ArgoCD from `infrastructure` to `apps`
    Signed-off-by: Alexandre Nicolaie <xunleii@users.noreply.github.com>

--- projects/chezmoi.sh/src/kubevault/kvstore.enc/cloud/tailscale/kubernetes.nx.chezmoi.sh
+++ projects/chezmoi.sh/src/kubevault/kvstore.enc/cloud/tailscale/kubernetes.nx.chezmoi.sh
@@ -1,6 +1,7 @@
-#ENC[AES256_GCM,data:rG7sOwC94HutBEmui9THfLpeiaNpbHL53TYC7ifOgfYrdplvmy64EgNBDF2K5FoF26+H4MMH9lNzoBblVqHy1D0TNkYDdETdDM1pLXyXx84FNHb32Q==,iv:qPoc8BLOqKIaWU45v5iNO3fF4Xv29WqtVCrtO0Sa9ww=,tag:FKgn2sojv7DRjdjWTnbjwQ==,type:comment]
-operator-oauth-client-id: ENC[AES256_GCM,data:EleGa/qMt+ykza99GB1Npa4=,iv:cOFQ2z3qaNhpdlG/y6UbZDL6CVNahdu+r4QdGyZmZPg=,tag:HYuTUfTeE5yqeQWyHVGsOg==,type:str]
-operator-oauth-client-secret: ENC[AES256_GCM,data:Y/txwtkjH716YX9iloiAIe9jHVnhZOf/0hUtzXc47F7H9geZ4C6j9TNOXmuc8eGdFIAbIJ28UYaCqMWffygZFA==,iv:jE2Jn03mCoptT5WBwO4LUMCQ/nP22eo0NspyCu2zIII=,tag:wPegbuGY/ZCVrDucMnPSjg==,type:str]
+#ENC[AES256_GCM,data:6i5/rlJ8xK4S6MLFoYsPdmPHPkd1yxacY2T3sIzCU2KK/AJop2jyrKPorePxwtyzQncCjcM3Q/HkVwzd9qTpo3ZIUWlVkstERuwaoveFqRtDJ/KQ,iv:zp+DUfQ76zePu5zG/+3tlxTiBvIqWbpyr5usSVKZQIk=,tag:ViGZq3yusppEzrw30sZHWg==,type:comment]
+#ENC[AES256_GCM,data:mVcNicq+Ai7GGZDlUyEOVAVmDayu1/fcmbLIeAHodCu0/GDWLNHH/WCtYYjtgG5O2ufg5S2Y0+wk3DY5rHwCGDn5E9pGytRzSGnoCGoJjyj5d+hASg==,iv:TxB9iJylWR5pan/x4ZdipWOnW2DcZel33UKV8ruV6q8=,tag:DKGXhyDaU8xSkx48RW/b0g==,type:comment]
+operator-oauth-client-id: ENC[AES256_GCM,data:SKrNiQ0cfj2aC9KEGXrUMbQ=,iv:/tbHSHi3wZT7D6dDDFVc09B0N5yRYYOzUeRBNqF8igg=,tag:UxC4nR7hE+EUSNqqZOI7xQ==,type:str]
+operator-oauth-client-secret: ENC[AES256_GCM,data:CjD3x7MoZeH4QgVOKoo+4OALAew5AIYwZFyQwUduesdTc7P2336Sz4jtSLahWkfLHF9RsLxErysthAfV+pymPg==,iv:HAWRlHAgGmSxXMNLtFdk0ydUUYZgN6XtxUGSV2Clp6U=,tag:oSBzfX9WfyU63fp/XBJXAQ==,type:str]
 sops:
     kms: []
     gcp_kms: []
@@ -10,14 +11,14 @@ sops:
         - recipient: age1fj0yj3na3n5udfjmnxfwrlkp80tvj49w80wh699x33dh48clnvnshtjxe9
           enc: |
             -----BEGIN AGE ENCRYPTED FILE-----
-            YWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBUeGdSaHk4Tks0a3BZZVc5
-            YWxueHIxalR1NXpkQ1BJNUdYT29ya2dneFQ0CktWRTRTaU9TdFhPTTl1Uk5EdGxL
-            UFVneVRrOUNyWDNUU1cyWHY3UGNFZ0kKLS0tIEF2VzFyVERLRUt6bFpJZWNwNVJH
-            K2RLbGRCUGw1blhRZWpYbGpxdmdncXcKEEYlVS2i1lxYN4kzS554u4r33djDKr3v
-            Id/yfM2enaKZ7z0vGKTLnDJ7ik/ND5GmPblQNpf3bxuA3w2cCOv4KA==
+            YWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBueU1vTHR0VVhWZmNra2Vu
+            Zlg1RktYdVJVUFEzRHBybjFzVTdibWxOUFFjCkdValFWZW04N0FwdkZmNEUycVlr
+            anJXMTNJQ1FkZEdUWGpPSTJ1VExZMTAKLS0tIGF5TFZ6TVZpT1d0U3lQbmVwNVhZ
+            ekdvMEJCaWhUb2w1NWFDRzBGT0M0czAKxvQanxcYGql8W66P2M1veT7bAiTsGsTY
+            52LJS+Nn46iaUNcIYEGP8DAP0tIAId66vMhxGcJr1XatxMh+4+HSqg==
             -----END AGE ENCRYPTED FILE-----
-    lastmodified: "2024-12-23T15:35:51Z"
-    mac: ENC[AES256_GCM,data:nzYnO8b+IVsGaU1S3cXqJUI4oF7x84ZAKNsh1C1FSWX9mDemM1u1Tjbt6w53Q4rLUoA/egSjTqYfZvI8GBev5cy60RYIqcx40R3spbuYpRagbJFXRvZ9DSuLwMTLoKHs7OEgYq62nZJ3ZpzKYzJgjXjMZIcm23PTmcdSubf/oUE=,iv:ILGkaBr1xSEpMrhm2zC8BPA8hxzJ/3Ld6xjtWXAd2vs=,tag:H1AJAC9+MT6AJtMBOLwx6Q==,type:str]
+    lastmodified: "2025-03-23T19:56:45Z"
+    mac: ENC[AES256_GCM,data:3xjTS2wF0ASTDmzEfbvPlMS8beEd0LRtLM/qKJG3PxBkekRfOPUJSBxp0WbfOxUj5+JFgCiTAcbsfCokUYdrsX0HhGq3v+oNYafvUCJXs+Rhxxk1t5BgUuMJ8901QdsaSkdM8d3jRzOZ2hQFDmcx88QDYAFqnHL7x30kKPZReR4=,iv:B6DUA3DmWF02gk+1Aue+/hMuZAKRJlXOHtQzikAj8oE=,tag:A0fBBXlcSS+1Qo6i5DYbiA==,type:str]
     pgp: []
     unencrypted_suffix: _unencrypted
-    version: 3.9.0
+    version: 3.9.1
