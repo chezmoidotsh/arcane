72d789e61dcd778b7f57db30622c22300b784391
Alexandre NICOLAIE <xunleii@users.noreply.github.com>
2026-03-03 21:17:08 +0100

    fixup! aa65dc5be14bf5f916499fd20633f45027617da1

--- projects/endfield.akn/src/infrastructure/nix/modules/whisper.nix
+++ projects/endfield.akn/src/infrastructure/nix/modules/whisper.nix
@@ -47,7 +47,6 @@ in
         "${whisper.models}/ggml-model.bin"
         "--inference-path"
         "/audio/transcriptions"
-        "--convert"
       ];
       WorkingDirectory = "${xdg.tmp}/whisper";
       # User
