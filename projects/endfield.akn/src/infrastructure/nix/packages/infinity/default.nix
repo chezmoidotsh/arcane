31fc337e0fd4fbaa0789e47721bc9c7fae379e9a
Alexandre NICOLAIE <xunleii@users.noreply.github.com>
2026-03-07 11:43:25 +0100

    fixup! 265ed679a94b0ea39969d13714958de56cfd138e

--- projects/endfield.akn/src/infrastructure/nix/packages/infinity/default.nix
+++ projects/endfield.akn/src/infrastructure/nix/packages/infinity/default.nix
@@ -86,6 +86,8 @@ let
       optimum = pkgs.python312Packages.optimum;
       gputil = dummyPkg "gputil";
       setuptools = pkgs.python312Packages.setuptools;
+      huggingface-hub = pkgs.python312Packages.huggingface-hub;
+      tokenizers = pkgs.python312Packages.tokenizers;
 
       # Additionally, sentence-transformers uses poetry-core, which sometimes needs to be explicit
       sentence-transformers = super.sentence-transformers.overridePythonAttrs (old: {
